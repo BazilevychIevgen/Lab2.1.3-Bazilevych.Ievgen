70c5a5cdb6573efb0e807ff0918953a88f96cda5
Admin <baro@ikrt>
2016-02-22 15:37:05 +0200

    Lab2.1.3

---
+++ src/edu/brainacad/Main.java
@@ -0,0 +1,19 @@
+package edu.brainacad;
+
+/**
+ * Created by Admin on 22.02.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Object[] object1 = new Object []{"a","b","c","d","e"};
+
+
+        for (Object element : object1){
+            System.out.println(element);
+
+        }
+    }
+
+}
